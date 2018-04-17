5f260568d22dc9df2ae92d51eea8e96ba4397987
jishubu <jishubu@qq.com>
2018-04-17 17:38:08 +0800

    Initial Commit

---
+++ novelRead/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  novelRead
+//
+//  Created by jishubu on 2018/4/17.
+//  Copyright © 2018年 jishubu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ novelRead/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  novelRead
+//
+//  Created by jishubu on 2018/4/17.
+//  Copyright © 2018年 jishubu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
